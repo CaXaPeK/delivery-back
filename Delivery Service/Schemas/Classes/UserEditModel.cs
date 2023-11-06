72ae3a9ab4f2be29e5944a436a95d33d7d382f99
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 18:35:51 +0700

    Создал класс UserEditModel

---
+++ Delivery Service/Schemas/Classes/UserEditModel.cs
@@ -0,0 +1,22 @@
+﻿using Delivery_Service.Schemas.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class UserEditModel
+    {
+        [Required]
+        [MinLength(1)]
+        public string fullName { get; set; }
+
+        public DateTime birthDate { get; set; }
+
+        [Required]
+        public Gender gender { get; set; }
+
+        public Guid addressId { get; set; }
+
+        [Phone]
+        public string phoneNumber { get; set; }
+    }
+}
