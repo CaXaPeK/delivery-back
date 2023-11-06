fe539f4f726a8583830098e2894ca39885dcaec9
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 18:29:58 +0700

    Сделал класс UserDto

---
+++ Delivery Service/Schemas/Classes/UserDto.cs
@@ -0,0 +1,29 @@
+﻿using Delivery_Service.Schemas.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class UserDto
+    {
+        public Guid id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string fullName { get; set; }
+
+        public DateTime birthDate { get; set; }
+
+        [Required]
+        public Gender gender { get; set; }
+
+        public Guid address {  get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MinLength(1)]
+        public string email { get; set; }
+
+        [Phone]
+        public string phoneNumber { get; set; }
+    }
+}
