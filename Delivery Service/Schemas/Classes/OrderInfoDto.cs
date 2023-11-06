8e140786cf4875086c91187fc8a89581a3ad85b3
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 17:27:02 +0700

    Создал класс OrderInfoDto

---
+++ Delivery Service/Schemas/Classes/OrderInfoDto.cs
@@ -0,0 +1,22 @@
+﻿using Delivery_Service.Schemas.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class OrderInfoDto
+    {
+        public Guid id { get; set; }
+
+        [Required]
+        public DateTime deliveryTime { get; set; }
+
+        [Required]
+        public DateTime orderTime { get; set; }
+
+        [Required]
+        public OrderStatus status { get; set; }
+
+        [Required]
+        public double price { get; set; }
+    }
+}
