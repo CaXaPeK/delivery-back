8c394cbb83450b816805a09dd50d61419c4c9f17
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 18:25:27 +0700

    Добавил класс TokenResponse

---
+++ Delivery Service/Schemas/Classes/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class TokenResponse
+    {
+        [Required]
+        [MinLength(1)]
+        public string token { get; set; }
+    }
+}
