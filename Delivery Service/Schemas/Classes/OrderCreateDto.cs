d50245ab7e4adfe8090e419b2e74d5334c43b9d2
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 17:13:59 +0700

    Создал класс OrderCreateDto

---
+++ Delivery Service/Schemas/Classes/OrderCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class OrderCreateDto
+    {
+        [Required]
+        public DateTime deliveryTime { get; set; }
+
+        public Guid addressId { get; set; }
+    }
+}
