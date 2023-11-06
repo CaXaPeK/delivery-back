3f8b9477fd40ef7baea066b8119d752362cacf66
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2023-11-06 17:11:27 +0700

    Добавил класс LoginCredentials

---
+++ Delivery Service/Schemas/Classes/LoginCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Delivery_Service.Schemas.Classes
+{
+    public class LoginCredentials
+    {
+        [Required]
+        [EmailAddress]
+        [MinLength(1)]
+        public string email { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string password { get; set; }
+    }
+}
